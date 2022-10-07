a5b67da37e0eeadbfca9c521617abce037901018
Eligh2022 <illia.parhom@gmail.com>
2022-10-07 15:50:03 +0300

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-map';
-}
+
+  onClear()  {
+   this.title = '';
+  }
+
+  onAdd() {
+    this.title = 'iluha.neffor'
+  }
+
+  }
+
+
